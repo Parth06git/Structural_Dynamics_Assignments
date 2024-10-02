9b18353115ddd3a94e4d1e95d58d2737034ccc60
Parth@Trivedi <ce21b024@iittp.ac.in>
2024-10-02 19:59:45 +0530

    Assignment 2 Completed

--- Assignment_2/Question3.py
+++ Assignment_2/Question3.py
@@ -19,15 +19,15 @@ force = np.zeros_like(t)
 force[t <= td] = F0
 
 # Central Difference Method
-p = np.zeros_like(t)
-x = np.zeros_like(t)
+p = []
+x = []
 u_prev = 2
-x[0] = 0
-p[0] = force[0] - 400*u_prev + 250*x[0]
-x[1] = p[0]/600
+x.append(0)
+p.append(force[0] - 400*u_prev + 250*x[0])
+x.append(p[0]/600)
 for i in range(1, len(t)-1):
-    p[i] = force[i] - 400*x[i-1] + 250*x[i]
-    x[i+1] = p[i]/600
+    p.append(force[i] - 400*x[i-1] + 250*x[i])
+    x.append(p[i]/600)
 
 
 # Interpolation of Excitation Method
@@ -39,25 +39,26 @@ A1 = -0.94
 B1 = 0.168
 C1 = 0.0005
 D1 = 0.0008
-u = np.zeros_like(t)
-v = np.zeros_like(t)
-
-x[0] = v[0] = 0
+u = []
+v = []
+u.append(0)
+v.append(0)
 
 for i in range(0, len(t)-1):
-    u[i+1] = A*u[i] + B*v[i] + C*force[i] + D*force[i+1]
-    v[i+1] = A1*u[i] + B1*v[i] + C1*force[i] + D1*force[i+1]
-    
+    u.append(A*u[i] + B*v[i] + C*force[i] + D*force[i+1])
+    v.append(A1*u[i] + B1*v[i] + C1*force[i] + D1*force[i+1])
+
 # Theoretical Displacement
-theoretical_disp = np.zeros_like(t)
+theoretical_disp = []
 for i, ti in enumerate(t):
     when = 0
     if ti <= td:
-        theoretical_disp[i]= (F0/k)*(1 - np.exp(-zeta*Wn*ti)*(np.cos(Wd*ti) + (zeta/np.sqrt(1-zeta**2))*np.sin(Wd*ti)))
+        theoretical_disp.append((F0/k)*(1 - np.exp(-zeta*Wn*ti)*(np.cos(Wd*ti) + (zeta/np.sqrt(1-zeta**2))*np.sin(Wd*ti))))
         if ti == td:
             when = i
+            print(theoretical_disp[when])
     else:
-        theoretical_disp[i] = np.exp((-c*(ti-td))/(2*m)) * (theoretical_disp[when]*np.cos(Wd*(ti-td)) + (1/Wd)*(-5.392 + (c*theoretical_disp[when]/(2*m)))*np.sin(Wd*(ti-td)))
+        theoretical_disp.append(np.exp(-0.2*(ti-td)) * (2.79*np.cos(Wd*(ti-td)) - 4.028*np.sin(Wd*(ti-td))))
 
 plt.figure(figsize=(10, 6))
 plt.plot(t, u, label='Numerical (Interpolation of Excitation)', marker='+')
